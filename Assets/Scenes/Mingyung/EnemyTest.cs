e855d0a6c24c747a9bf0896329f31fe4ff0ae739
mingyung452 <alsrud04228@naver.com>
2024-11-07 00:10:42 +0900

    enemy엑셀

---
+++ Assets/Scenes/Mingyung/EnemyTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyTest 
+{
+    public string enemy_name;
+    public string HP;
+    public string attack;
+    public string speed;
+    public string defense;
+    public string stiffness_resistance;
+}
